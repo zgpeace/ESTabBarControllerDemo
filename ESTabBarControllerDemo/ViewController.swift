54da92a0dd5843e34cc08de33e5d61248add55ac
zgpeace <zgpeace@gmail.com>
2017-05-26 10:33:11 +0800

    Initial Commit

---
+++ ESTabBarControllerDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ESTabBarControllerDemo
+//
+//  Created by zgpeace on 26/05/2017.
+//  Copyright © 2017 zgpeace. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ESTabBarControllerDemoTests/ESTabBarControllerDemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  ESTabBarControllerDemoTests.swift
+//  ESTabBarControllerDemoTests
+//
+//  Created by zgpeace on 26/05/2017.
+//  Copyright © 2017 zgpeace. All rights reserved.
+//
+
+import XCTest
+@testable import ESTabBarControllerDemo
+
+class ESTabBarControllerDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
